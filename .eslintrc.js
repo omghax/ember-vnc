c1bd8dd4cb5e505d2797a415491d27af3b6d6532
Dray Lacy <dray@envylabs.com>
2019-04-20 22:20:17 -0400

    build: add commitlint and commitizen (and git hooks)
    This enforces the conventional-changelog standard for commit messages.

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,6 +22,7 @@ module.exports = {
       files: [
         '.eslintrc.js',
         '.template-lintrc.js',
+        'commitlint.config.js',
         'ember-cli-build.js',
         'index.js',
         'testem.js',
